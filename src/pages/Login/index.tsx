08aca44885b380768569930a4629d59c24aab602
Renan Cesar <cesar.renan@hotmail.com>
2023-04-23 15:01:18 -0300

    Resolvendo problema com lotties

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,8 +1,16 @@
+import * as Lottieplayer from "@lottiefiles/lottie-player";
+import "@lottiefiles/lottie-player";
 
 const Login = () => {
   return (
     <div>
       <h1>Login</h1>
+      <div style={{
+        width: '300px',
+      }}>
+        <lottie-player src="https://assets5.lottiefiles.com/packages/lf20_uxobbjkj.json"  background="transparent"  speed="1"  styles="width: 300px; height: 300px;"  loop autoplay></lottie-player>
+        {/* <lottie-player src="https://assets1.lottiefiles.com/packages/lf20_puciaact.json"  background="transparent"  speed="1"  styles="width: 300px; height: 300px;"  loop autoplay></lottie-player> */}
+      </div>
     </div>
   )
 }
